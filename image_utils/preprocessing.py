5fd0e21fd01a1fa84def31eb65fd6ac75bfd0eca
BrianP8701 <brianp8701@gmail.com>
2023-06-09 00:24:28 -0400

    MobileNet models

--- image_utils/preprocessing.py
+++ image_utils/preprocessing.py
@@ -3,9 +3,15 @@ This file contains functions for preprocessing images.
 
     - Add padding to square images to make them larger.
     - Resize square images.
+    - Simplify images by emphasizing edges and reducing noise through color simplification based on pixel color variation.
+        Before inputting image, make sure to make it black and white. For example, you can do:
+        image = cv2.imread(image_path)[:,:,0]
+
 '''
 
 from PIL import Image, ImageOps
+import numpy as np
+import cv2
 
 # Simply rescales images to a square of size x size pixels. Meant to work with square images.
 def resize_image(input_path, output_path, size):
@@ -29,13 +35,62 @@ def pad_image(input_path, output_path, final_size):
         img_with_border = ImageOps.expand(img, (left_padding, top_padding, right_padding, bottom_padding), fill='black')
         img_with_border.save(output_path)
 
+# The method simplifies the image by emphasizing edges and reducing noise through color simplification based on pixel color variation.
+def simplify(img: np.ndarray):
+    # Get standard deviation across all pixels in image
+    x = np.std(img)
+    
+    # Compute the 'range' threshold based on the standard deviation
+    rang = 2.04023 * x - 4.78237
+    if(x < 20): rang = 5
+    print(rang)
+    
+    # Find the maximum pixel intensity in the image
+    whitestPixel = 0
+    for i in range(len(img)):
+        for j in range(len(img[0])):
+            if(img[i][j] > whitestPixel): whitestPixel = img[i][j]
+            
+    # Set all pixels with intensities greater than 'whitestPixel - range' to 255 (white)
+    for i in range(len(img)):
+        for j in range(len(img[0])):
+            if(img[i][j] > whitestPixel - rang): img[i][j] = 255
+            
+    # Set cutoff to '255 - range'
+    cutoff = 255 - rang
+    
+    # Loop until all pixels have been categorized
+    while(True):    
+        whitestPixel = 0
+        
+        # Find the maximum pixel intensity that's less than 'cutoff' and greater than the most intense pixel in this range
+        for i in range(len(img)):
+            for j in range(len(img[0])):
+                if(img[i][j] < cutoff and img[i][j] > whitestPixel): whitestPixel = img[i][j]
+                
+        # Break the loop if no such pixel is found
+        if whitestPixel == 0: break
+        
+        # Set all pixels with intensities greater than 'whitestPixel - range' and less than 'cutoff' to 'whitestPixel'
+        for i in range(len(img)):
+            for j in range(len(img[0])):
+                if(img[i][j] > whitestPixel - rang and img[i][j] < cutoff): img[i][j] = whitestPixel
+                
+        # Reduce 'cutoff' by 'range'
+        cutoff = whitestPixel - rang
+    return img
+
 
 frame = 0
-size = 224  # size of the output image in pixels
 while True:
-    input_path = f'bubble_datasets/bubble/frame{frame}.jpg'
-    output_path = f'bubble_resize_datasets/bubble/frame{frame}.jpg'
-    resize_image(input_path, output_path, size)
+    print(frame)
+    input_path = f'datasets/resize_datasets/normal/frame{frame}.jpg'
+    output_path = f'whiten1_224/frame{frame}.jpg'
+    
+    image = cv2.imread(input_path)[:,:,0]
+    image = simplify(image)
+    cv2.imwrite(output_path, image)
+    print()
+    
     frame += 1
-
 
