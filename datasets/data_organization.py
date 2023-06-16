821a4dde6e55018247fd683adaca940d4abe26d4
BrianP8701 <brianp8701@gmail.com>
2023-06-16 01:26:43 -0400

    Fixed indices of images. Added method to do this

--- datasets/data_organization.py
+++ datasets/data_organization.py
@@ -29,6 +29,33 @@ def train_val_split(source_dir, target_dir, val_size):
             for img in val_images:
                 shutil.move(os.path.join(source_dir, class_dir, img), os.path.join(target_dir, 'val', class_dir, f'frame{frame}.jpg'))
                 frame += 1
+                
+'''            
+If you have a folder of images formatted like this:
+    frame5.jpg
+    frame7.jpg
+    frame13.jpg
+    ...
+You can use this method to order them into:
+    frame0.jpg
+    frame1.jpg
+    frame2.jpg
+    ...
+'''
+
+def rename_files(directory_path):
+    # Get a list of all files in the directory
+    files = os.listdir(directory_path)
+    
+    # Sort the list of files based on the number in the file name
+    files.sort(key=lambda f: int(f.split('frame')[1].split('.jpg')[0]))
+    
+    # Iterate over the sorted list and rename each file
+    for i, file in enumerate(files):
+        old_path = os.path.join(directory_path, file)
+        new_path = os.path.join(directory_path, f'frame{i}.jpg')
+        os.rename(old_path, new_path)
+
 
 source_dir = 'datasets/resize_datasets'
 target_dir = 'datasets/resize'
